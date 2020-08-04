c99b1a9d3399992450ce1b6e54019bff2172e17b
Rishabh Thurkal <rishi.feb1@gmail.com>
2020-08-04 05:27:22 +0200

    Added db models

--- config.py
+++ config.py
@@ -2,11 +2,12 @@
 # @Author: rish
 # @Date:   2020-08-04 00:17:37
 # @Last Modified by:   rish
-# @Last Modified time: 2020-08-04 04:08:23
+# @Last Modified time: 2020-08-04 04:59:01
 
 
 ### Imports START
 import datetime
+import os
 ### Imports END
 
 DATE_FORMAT = '%Y-%m-%d'
@@ -14,3 +15,15 @@ DATE_LOWER_BOUND = '2000-01-01'
 DATE_UPPER_BOUND = datetime.date.today().strftime(DATE_FORMAT)
 LATEST_LOWER_BOUND = 7
 EXCHANGE_RATES_API_URL = 'https://api.exchangeratesapi.io/{date}'
+
+if os.environ.keys().__contains__('ENV-INDICATOR') \
+	and os.environ['ENV-INDICATOR'] == 'PROD':
+	# Environment string
+	env_str = 'PROD'
+	DB_CONN_STRING = ''
+	BASE_PATH = os.environ['SCPATH']
+else:
+	# Environment string
+	env_str = 'DEV'
+	DB_CONN_STRING = 'mysql+mysqlconnector://rish@localhost/pd_exchangerates'
+	BASE_PATH = ''
--- er_extractor/models.py
+++ er_extractor/models.py
@@ -2,4 +2,85 @@
 # @Author: rish
 # @Date:   2020-08-02 23:09:08
 # @Last Modified by:   rish
-# @Last Modified time: 2020-08-04 00:10:13
+# @Last Modified time: 2020-08-04 05:07:12
+
+
+### Imports START
+from sqlalchemy import create_engine, UniqueConstraint
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, BigInteger, Integer, DateTime, Date,\
+	JSON, Numeric
+from sqlalchemy.sql import func
+### Imports END
+
+
+# Object for the delcaratove base class
+Base = declarative_base()
+
+### Model Class to define the data base structure START
+
+
+# [START ExchangeRate model to define the exchange_rates table]
+class ExchangeRate(Base):
+	'''
+	Model class that defines the structure for the exchange rates
+	table in the database.
+	'''
+
+	__tablename__ = 'exchange_rates'
+
+	_id = Column(BigInteger, primary_key=True)
+
+	date = Column(Date, nullable=False, unique=True)
+	base = Column(String, nullable=False)
+
+	USD = Column(Numeric)
+	CAD = Column(Numeric)
+	INR = Column(Numeric)
+	SGD = Column(Numeric)
+	RUB = Column(Numeric)
+	CZK = Column(Numeric)
+	ISK = Column(Numeric)
+	HKD = Column(Numeric)
+
+	rates_payload = Column(JSON, nullable=False)
+	request_status = Column(Integer, nullable=False)
+
+	insert_time = Column(DateTime, nullable=False, default=func.now())
+	update_time = Column(
+		DateTime, nullable=False,
+		default=func.now(),
+		onupdate=func.current_timestamp()
+	)
+
+	UniqueConstraint(
+		date, base,
+		name='unique_date_base_exchange_rates'
+	)
+# [END]
+
+
+# [START Code to create the database from all the above schema classes]
+def convert_classes_into_tables(connection_string):
+	'''
+	Function that maps and creates the tables in the databse from all
+	the above schema.
+
+	Args:
+		- connection string
+	Returns:
+		-
+	'''
+
+	engine = create_engine(connection_string)
+	Base.metadata.create_all(engine)
+	print('created_tables')
+	return
+# [END]
+
+
+### Creating schema in database START
+if __name__ == '__main__':
+	import context
+	convert_classes_into_tables(context.config.DB_CONN_STRING)
+### Creating schema in database END
